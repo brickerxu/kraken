e91fc1add57bf6b9bd453d8702c6cf05844a4f53
brickerxu <brickerxu@163.com>
2020-02-02 12:33:35 +0800

    显示窗体

--- src/windows/__init__.py
+++ src/windows/__init__.py
@@ -2,3 +2,5 @@
 # @author bricker
 # @date 2019/10/16
 # @file __init__.py
+
+from .reptile import Reptile
--- src/windows/reptile.py
+++ src/windows/reptile.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # @author bricker
 # @date 2019/10/16
-# @file Reptile.py
+# @file reptile.py
 
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5.QtWidgets import QMessageBox
@@ -10,4 +10,4 @@ from PyQt5.QtGui import QTextCursor
 
 class Reptile(QMainWindow):
     def __init__(self, parent=None):
-        pass
+        super(Reptile, self).__init__(parent)
