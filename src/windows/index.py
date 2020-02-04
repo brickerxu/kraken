b477e1ff89e14e2bd5635428bd1daace8cdd8172
brickerxu <brickerxu@163.com>
2020-02-04 22:14:38 +0800

    显示搜索结果列表

--- src/windows/index.py
+++ src/windows/index.py
@@ -3,8 +3,7 @@
 # @date 2019/10/16
 # @file index.py
 
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QTreeWidget, QTreeWidgetItem
 from PyQt5.QtGui import QTextCursor
 from ui import Ui_index
 from resolver import resolvers
@@ -22,7 +21,21 @@ class Index(QMainWindow, Ui_index):
     def search_click(self):
         keyword = self.edit_keyword.text()
         if not keyword or keyword is None:
-            QMessageBox.information(self, "提示", "关键字为空！", QMessageBox.Yes)
+            QMessageBox.information(self, "提示", "关键字为空！", QMessageBox.Ok)
             return
+        self.search_result.setHidden(False)
+        self.tree_search.clear()
         for r in resolvers:
+            item = QTreeWidgetItem()
+            item.setText(0, r.name())
+
             result = r.search(keyword)
+
+            cartoons = result._res
+
+            for c in cartoons:
+                child = QTreeWidgetItem()
+                child.setText(0, c._name)
+                item.addChild(child)
+
+            self.tree_search.insertTopLevelItem(0, item)
